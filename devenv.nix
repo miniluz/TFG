e7a3d194ce9081084f665e0414d84985421402fc
miniluz <javiermelon4fu@gmail.com>
2025-09-12 20:25:55 +0200

    feat: :sparkles: Basic devenv

---
+++ devenv.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  packages = with pkgs; [
+    git
+    cargo-binutils
+    cargo-generate
+    gdb
+    openocd
+    usbutils
+    # qemu_full
+  ];
+
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    targets = [
+      "x86_64-unknown-linux-gnu"
+      "thumbv7em-none-eabihf"
+    ];
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+      "llvm-tools"
+    ];
+  };
+
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+  };
+
+}
