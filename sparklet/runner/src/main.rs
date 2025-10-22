01b7a8faeaab51451b41bb09339bafecb7ddef6a
miniluz <javiermelon4fu@gmail.com>
2025-10-22 10:17:28 +0200

    Add task to listen to MIDI on the UART pin

--- sparklet/runner/src/main.rs
+++ sparklet/runner/src/main.rs
@@ -1,13 +1,27 @@
 #![no_std]
 #![no_main]
 
+mod hardware;
+mod midi_task;
+
 use defmt::info;
+use embassy_executor::Executor;
+use static_cell::StaticCell;
+
 use defmt_rtt as _;
 use embassy_stm32 as _;
 use panic_probe as _;
 
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
+    let hardware = hardware::Hardware::get();
+
+    let executor = EXECUTOR.init(embassy_executor::Executor::new());
+
+    midi_task::setup_task(executor, hardware.midi_uart_buffered);
+
     info!("Hello, world!");
 
     panic!();
---
+++ sparklet/runner/src/midi_task.rs
@@ -0,0 +1,66 @@
+use defmt::trace;
+use embassy_executor::Executor;
+use embassy_stm32::usart::RingBufferedUartRx;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use midi::{MidiEvent, MidiListener};
+use static_cell::StaticCell;
+
+pub const MIDI_CHANNEL_SIZE: usize = 4;
+
+pub struct MidiTaskState<'a> {
+    midi_listener: MidiListener<'a, CriticalSectionRawMutex, MIDI_CHANNEL_SIZE>,
+    midi_uart_buffered: RingBufferedUartRx<'a>,
+}
+
+impl<'a> MidiTaskState<'a> {
+    pub fn new(
+        midi_listener: MidiListener<'a, CriticalSectionRawMutex, MIDI_CHANNEL_SIZE>,
+        midi_uart_buffered: RingBufferedUartRx<'a>,
+    ) -> MidiTaskState<'a> {
+        MidiTaskState {
+            midi_listener,
+            midi_uart_buffered,
+        }
+    }
+}
+
+pub static MIDI_TASK_STATE: StaticCell<MidiTaskState> = StaticCell::new();
+
+pub static MIDI_TASK_CHANNEL: Channel<CriticalSectionRawMutex, MidiEvent, MIDI_CHANNEL_SIZE> =
+    Channel::new();
+
+pub fn setup_task(
+    executor: &'static mut Executor,
+    midi_uart_buffered: RingBufferedUartRx<'static>,
+) {
+    let midi_sender = MIDI_TASK_CHANNEL.sender();
+
+    let midi_listener = MidiListener::new(midi_sender);
+
+    executor.run(|spawner| {
+        spawner
+            .spawn(midi_task(
+                MIDI_TASK_STATE.init(MidiTaskState::new(midi_listener, midi_uart_buffered)),
+            ))
+            .ok();
+    });
+}
+
+#[embassy_executor::task]
+pub async fn midi_task(state: &'static mut MidiTaskState<'static>) {
+    let mut buffer = [0; 1];
+    loop {
+        match state.midi_uart_buffered.read(&mut buffer).await {
+            Ok(1) => state.midi_listener.process_bytes(&buffer),
+            Ok(other_size) => {
+                trace!(
+                    "Unexpected number of bytes read on MIDI UART: {}",
+                    other_size
+                );
+            }
+            Err(err) => {
+                trace!("Error reading from MIDI UART: {}", err);
+            }
+        };
+    }
+}
