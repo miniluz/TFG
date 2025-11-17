1d4057f441f8378a3d44b92b6385574cad4d7ddc
miniluz <javiermelon4fu@gmail.com>
2025-11-17 10:30:47 +0100

    Set up document

--- flake.nix
+++ flake.nix
@@ -32,15 +32,7 @@
               nativeBuildInputs = with pkgs; [
                 (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
 
-                (texlive.combine {
-                  inherit (texlive)
-                    scheme-basic
-                    # dvisvgm dvipng # for preview and export as html
-                    # wrapfig amsmath ulem hyperref capt-of
-                    #(setq org-latex-compiler "lualatex")
-                    #(setq org-preview-latex-default-process 'dvisvgm)
-                    ;
-                })
+                typst
 
                 cargo-binutils
                 cargo-expand
@@ -50,32 +42,7 @@
                 gdb
                 openocd
                 usbutils
-                (pkgs.rustPlatform.buildRustPackage rec {
-                  pname = "probe-rs-tools";
-                  version = "0.27.0";
-
-                  src = fetchFromGitHub {
-                    owner = "probe-rs";
-                    repo = "probe-rs";
-                    rev = "70f2e4060a81041f2dac7f388b5e11143696b3af";
-                    hash = "sha256-qpHSfn8/dvFC5yjtbD35j5OH2XJQrDI988V7Oidsr8A=";
-                  };
-
-                  cargoHash = "sha256-C5ca6uaZ6RBtLoShkat9OoaeNM7iEgiUltcwBCcvvQw=";
-
-                  buildAndTestSubdir = pname;
-
-                  nativeBuildInputs = [
-                    cmake
-                    pkg-config
-                  ];
-
-                  buildInputs = [
-                    libusb1
-                    openssl
-                  ];
-                  doCheck = false;
-                })
+                probe-rs-tools
               ];
               buildInputs = [
               ];
